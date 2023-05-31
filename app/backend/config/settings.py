ecb147e8e309b6eb0521cd267d380126faefc931
gustavo ramon dos reis <gustavobilireis@hotmail.com>
2023-05-30 21:14:28 -0300

    Feat: implementa gitignore

--- app/backend/config/settings.py
+++ app/backend/config/settings.py
@@ -1,4 +1,7 @@
-from pathlib import Path
+from pathlib import Path, os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m5_(i3y=5$0jkn$rbqh37(2gvvpbnj%km^--)1s)qvleqr=^x8"
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
